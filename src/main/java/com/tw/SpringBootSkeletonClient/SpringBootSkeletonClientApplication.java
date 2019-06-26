3f94e5c85fb5a2108b3660138c33fe48bca5ae6d
Harshad Thombare <in-harshad.thombare@Harshads-MacBook-Pro.local>
2019-06-26 10:35:41 +0530

    Harshad/Aditi - Add concumer contract test

---
+++ src/main/java/com/tw/SpringBootSkeletonClient/SpringBootSkeletonClientApplication.java
@@ -0,0 +1,14 @@
+package com.tw.SpringBootSkeletonClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSkeletonClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSkeletonClientApplication.class, args);
+	}
+
+
+}
